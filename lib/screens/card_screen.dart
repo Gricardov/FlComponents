d04e22e8128b9a550707d4ad9a2cf343666732b1
gricardov <gricardov@gmail.com>
2022-01-05 00:17:01 -0500

    card básico

--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -1,15 +1,31 @@
+import 'package:fl_components/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
 class CardScreen extends StatelessWidget {
-   
   const CardScreen({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-         child: Text('CardScreen'),
-      ),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Card Widget'),
+        ),
+        body: ListView(
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10), // Puede ser .all si todos van a ser iguales
+          children: [
+            Card(
+                child: Column(
+              children: const [
+                ListTile(
+                    leading: Icon(
+                      Icons.photo_album_outlined,
+                      color: AppTheme.primary,
+                    ),
+                    title: Text('Soy un tístulo'),
+                    subtitle: Text('Duis duis tempor et duis eu tempor est velit commodo tempor.'),),
+              ],
+            ))
+          ],
+        ));
   }
 }
