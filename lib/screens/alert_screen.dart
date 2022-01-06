79526a225d00da1a2655b19e19f630a8f85ac395
gricardov <gricardov@gmail.com>
2022-01-06 00:55:18 -0500

    material dialog

--- lib/screens/alert_screen.dart
+++ lib/screens/alert_screen.dart
@@ -3,15 +3,42 @@ import 'package:flutter/material.dart';
 class AlertScreen extends StatelessWidget {
   const AlertScreen({Key? key}) : super(key: key);
 
+  void displayDialog(BuildContext context) {
+    showDialog(
+        barrierDismissible: true,
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            elevation: 5,
+            //title: const Text('Título'),
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: const [
+                Text('Este es el contenido'),
+                SizedBox(
+                  height: 10,
+                ),
+                FlutterLogo(size: 100)
+              ],
+            ),
+            actions: [
+              TextButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: Text('Cancelar'))
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: ElevatedButton(
             // style: , Puedes sobreescribir el estilo del tema
-            onPressed: () {
-              Navigator.pop(context);
-            },
+            onPressed: () => displayDialog(context),
             child: const Padding(
               padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
               child: Text(
@@ -21,7 +48,8 @@ class AlertScreen extends StatelessWidget {
             )),
       ),
       floatingActionButton: FloatingActionButton(
-          child: const Icon(Icons.close), onPressed: () {}),
+          child: const Icon(Icons.close),
+          onPressed: () => Navigator.pop(context)),
     );
   }
 }
