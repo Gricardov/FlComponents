1d1a34448489fe3f60d20c4ef79cbc237c69bac3
gricardov <gricardov@gmail.com>
2022-01-08 01:35:56 -0500

    basic form

--- lib/screens/inputs_screen.dart
+++ lib/screens/inputs_screen.dart
@@ -6,6 +6,17 @@ class InputsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Usamos los keys de cada widget
+    final GlobalKey<FormState> myFormKey = GlobalKey<FormState>();
+
+    final Map<String, String> formValues = {
+      'first_name': 'Mila',
+      'last_name': 'Luna',
+      'email': 'sorgalim@gmail.com',
+      'password': '123456',
+      'role': "admin"
+    };
+
     return Scaffold(
         appBar: AppBar(
           title: const Text('Inputs form'),
@@ -13,40 +24,58 @@ class InputsScreen extends StatelessWidget {
         body: SingleChildScrollView(
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-            child: Column(
-              children: const [
-                CustomInputField(
-                  labelText: 'Nombre',
-                  hintText: 'Nombre del usuario',
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                CustomInputField(
-                  labelText: 'Apellido',
-                  hintText: 'Apellido del usuario',
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                CustomInputField(
-                  labelText: 'Correo',
-                  hintText: 'Correo del usuario',
-                  keyboardType: TextInputType.emailAddress,
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                CustomInputField(
-                  labelText: 'Contraseña',
-                  hintText: 'Contraseña del usuario',
-                  isPassword: true,
-                  keyboardType: TextInputType.emailAddress,
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-              ],
+            child: Form(
+              key: myFormKey,
+              child: Column(
+                children: [
+                  const CustomInputField(
+                    labelText: 'Nombre',
+                    hintText: 'Nombre del usuario',
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  const CustomInputField(
+                    labelText: 'Apellido',
+                    hintText: 'Apellido del usuario',
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  const CustomInputField(
+                    labelText: 'Correo',
+                    hintText: 'Correo del usuario',
+                    keyboardType: TextInputType.emailAddress,
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  const CustomInputField(
+                    labelText: 'Contraseña',
+                    hintText: 'Contraseña del usuario',
+                    isPassword: true,
+                    keyboardType: TextInputType.emailAddress,
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        // Desactivo el teclado
+                        FocusScope.of(context).requestFocus(FocusNode());
+                        // Valido el formulario
+                        if (!myFormKey.currentState!.validate()) {
+                          print('Formulario no válido');
+                          return;
+                        }
+                        print(formValues);
+                      },
+                      child: const SizedBox(
+                        width: double.infinity,
+                        child: Center(child: Text('Guardar')),
+                      ))
+                ],
+              ),
             ),
           ),
         ));
