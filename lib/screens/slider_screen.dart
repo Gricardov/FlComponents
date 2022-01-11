a6c05a366ccd7e45c5fe2796be4634119f0c86a3
gricardov <gricardov@gmail.com>
2022-01-11 00:39:47 -0500

    img with expanded and switches

--- lib/screens/slider_screen.dart
+++ lib/screens/slider_screen.dart
@@ -10,6 +10,7 @@ class SliderScreen extends StatefulWidget {
 
 class _SliderScreenState extends State<SliderScreen> {
   double _sliderValue = 100;
+  bool _sliderEnabled = true;
 
   @override
   Widget build(BuildContext context) {
@@ -17,30 +18,90 @@ class _SliderScreenState extends State<SliderScreen> {
         appBar: AppBar(
           title: const Text('Sliders and checks'),
         ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Slider.adaptive(
-                  min: 50,
-                  max: 400,
-                  activeColor: AppTheme.primary,
-                  //divisions: 10,
-                  value: _sliderValue,
-                  onChanged: (value) {
-                    setState(() {
-                      _sliderValue = value;
-                    });
-                    print(value);
-                  }),
-              Image(
-                image: const NetworkImage(
-                    'http://pngimg.com/uploads/batman/batman_PNG51.png'),
-                fit: BoxFit.contain,
-                width: _sliderValue,
+        body: Column(
+          children: [
+            Slider.adaptive(
+                min: 50,
+                max: 400,
+                activeColor: AppTheme.primary,
+                //divisions: 10,
+                value: _sliderValue,
+                onChanged: _sliderEnabled
+                    ? (value) {
+                        setState(() {
+                          _sliderValue = value;
+                        });
+                        print(value);
+                      }
+                    : null),
+
+            Checkbox(
+                value: _sliderEnabled,
+                onChanged: (value) {
+                  setState(() {
+                    _sliderEnabled = value ?? false;
+                  });
+                }),
+
+            CheckboxListTile(
+              activeColor: AppTheme.primary,
+              title: const Text('Habilitar slider'),
+              value: _sliderEnabled,
+              onChanged: (value) {
+                setState(() {
+                  _sliderEnabled = value ?? false;
+                });
+                print(value);
+              },
+            ),
+
+            Switch(
+                value: _sliderEnabled,
+                onChanged: (value) {
+                  setState(() {
+                    _sliderEnabled = value;
+                  });
+                }),
+
+            SwitchListTile(
+              activeColor: AppTheme.primary,
+              title: const Text('Habilitar slider'),
+              value: _sliderEnabled,
+              onChanged: (value) {
+                setState(() {
+                  _sliderEnabled = value;
+                });
+                print(value);
+              },
+            ),
+
+            SwitchListTile.adaptive(
+              activeColor: AppTheme.primary,
+              title: const Text('Habilitar slider'),
+              value: _sliderEnabled,
+              onChanged: (value) {
+                setState(() {
+                  _sliderEnabled = value;
+                });
+                print(value);
+              },
+            ),
+
+            const AboutListTile(),
+
+            Expanded(
+              // Esto es como un flex:1, porque toma el ancho restante
+              child: SingleChildScrollView(
+                child: Image(
+                  image: const NetworkImage(
+                      'https://cdn.pixabay.com/photo/2020/07/06/17/51/batman-5377804_960_720.png'),
+                  fit: BoxFit.contain,
+                  width: _sliderValue,
+                ),
               ),
-              SizedBox(height: 100,)
-            ],
-          ),
+            ),
+            //SizedBox(height: 100,)
+          ],
         ));
   }
 }
